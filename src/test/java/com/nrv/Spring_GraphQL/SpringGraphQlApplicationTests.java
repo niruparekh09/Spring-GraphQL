47427781680863b543db205143e72662c4a6b7ac
Nirav Parekh <niruparekh09@gmail.com>
2024-10-13 13:40:45 +0530

    :tada: first commit

---
+++ src/test/java/com/nrv/Spring_GraphQL/SpringGraphQlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nrv.Spring_GraphQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGraphQlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
